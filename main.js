a089cd1afd957c0f2a6a3a7fd34c415c13317e44
Hafsah14 <hafsah_tabasum@outlook.com>
2025-07-14 19:22:43 +0100

    set up empty board game

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// Noughts and Crosses Game
 
+// Create a 3x3 board
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+// Print the board
+board.forEach(row => {
+  console.log(row.join('|'));
+});
